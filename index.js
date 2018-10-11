73338fa7d533fdf297cd677e9ee0617d78162e51
Rafael Bastos <rafael.bastos@softec.com.py>
2018-10-11 16:02:06 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app')
+
+// ReactDOM.render(que voy renderizar, donde lo hare)
+const text = <h1>blablabla</h1>;
+ReactDOM.render(text , app);
