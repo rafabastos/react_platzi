312faea1ab03927444eaa61903c2a160a1e5762e
Rafael Bastos <rafael.bastos@softec.com.py>
2018-10-12 11:21:02 -0300

    accion de click en el component

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const app = document.getElementById('app')
 
 // ReactDOM.render(que voy renderizar, donde lo hare)
 // const text = <h1>blablabla</h1>;
-ReactDOM.render(<Media />, app);
+ReactDOM.render(<Media title="Qué es responsive Design" author="rafabastos" image="./images/covers/responsive.jpg"/>, app);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,22 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './media.css';
 
 class Media extends React.Component {
+	// constructor(props) {
+	// 	super(props)
+	// 	this.handleClick = this.handleClick.bind(this)
+	// }
+	handleClick = (event) => {
+		console.log(this.props.title);
+	}
 	render() {
+		const styles = {
+			container: {
+				color: '#44546b',
+				cursor: 'pointer',
+				width: 260,
+				border: '1px solid red'
+			}
+		}
 		return (
-			<div>
-                <div>
+			<div className="Media" onClick={this.handleClick}>
+                <div className="Media-cover">
 					<img
-						src=""
+						src={this.props.image}
 						alt=""
 						width={260}
 						height={160}
+						className="Media-image"
 					/>
-					<h3>titulo</h3>
-					<p>rafabastos</p>
+					<h3 className="Media-title">{this.props.title}</h3>
+					<p className="Media-author">rafabastos</p>
 				</div>
             </div>
 		)
 	}
+}
+
+Media.propTypes = {
+	image: PropTypes.string,
+	title: PropTypes.string,
+	author: PropTypes.string,
 }
 
 export default Media;
