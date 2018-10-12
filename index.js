d6fda5d5ad615b335bc6eb0405083a2720b413e6
Rafael Bastos <rafael.bastos@softec.com.py>
2018-10-12 09:16:12 -0300

    creacion de componentes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Media from './src/playlist/components/media.js'; 
 
 const app = document.getElementById('app')
 
 // ReactDOM.render(que voy renderizar, donde lo hare)
-const text = <h1>blablabla</h1>;
-ReactDOM.render(text , app);
+// const text = <h1>blablabla</h1>;
+ReactDOM.render(<Media />, app);
---
+++ src/playlist/components/media.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Media extends React.Component {
+	render() {
+		return (
+			<div>
+                <div>
+					<img
+						src=""
+						alt=""
+						width={260}
+						height={160}
+					/>
+					<h3>titulo</h3>
+					<p>rafabastos</p>
+				</div>
+            </div>
+		)
+	}
+}
+
+export default Media;
