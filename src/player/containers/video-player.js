ed4d13f89a5ba28720ee35b4913d431222b84d79
Rafael Bastos <rafael.bastos@softec.com.py>
2018-10-15 16:30:15 -0300

    barra de progresso

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -3,10 +3,17 @@ import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
 import PlayPause from '../components/play-pause';
+import Timer from '../components/timer';
+import Controls from '../components/video-player-controls';
+import ProgressBar from '../components/progress-bar';
+import Spinner from '../components/spinner';
 
 class VideoPlayer extends Component {
     state = {
         pause: true,
+        duration: 0,
+        currentTime: 0,
+        loading: false,
     }
     togglePlay = event => {
         this.setState({
@@ -18,20 +25,62 @@ class VideoPlayer extends Component {
             pause: (!this.props.autoPlay)
         })
     }
+    handleLoadedMetadata = event => {
+        this.video = event.target;
+        this.setState({
+            duration: this.video.duration
+        });
+    }
+    handleTimeUpdate = event => {
+        this.setState({
+            currentTime: this.video.currentTime
+        })
+    }
+    handleProgressChange = event => {
+        this.video.currentTime = event.target.value
+    }
+    handleSeeking = event => {
+        this.setState({
+            loading: true
+        })
+    }
+    handleSeeked = event => {
+        this.setState({
+            loading: false
+        })
+    }
     render() {
         return (
             <VideoPlayerLayout>
                 <Title 
                     title="soy un titulo"
                 />
-                <PlayPause 
-                    pause={this.state.pause}
-                    handleClick={this.togglePlay}
+                <Controls>
+                    <PlayPause 
+                        pause={this.state.pause}
+                        handleClick={this.togglePlay}
+                    />
+                    <Timer 
+                        duration={this.state.duration}
+                        currentTime={this.state.currentTime}
+                    />
+                    <ProgressBar 
+                        duration={this.state.duration}
+                        value={this.state.currentTime}
+                        handleProgressChange={this.handleProgressChange}
+                    />
+                </Controls>
+                <Spinner 
+                    active={this.state.loading}
                 />
                 <Video 
                     controls
                     autoPlay={this.props.autoPlay}
                     pause={this.state.pause}
+                    handleLoadedMetadata={this.handleLoadedMetadata}
+                    handleTimeUpdate={this.handleTimeUpdate}
+                    handleSeeking={this.handleSeeking}
+                    handleSeeked={this.handleSeeked}
                     src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
                 />
             </VideoPlayerLayout>
